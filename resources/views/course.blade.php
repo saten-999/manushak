0ebcaa1fca72a4f1fb07cc60e03e0416aef3177d
Satenik <satabr1999@gmail.com>
2022-04-12 00:07:42 +0400

    update

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -9,7 +9,8 @@
             <div class="col-3  mt-1">
                 
                 <a href="/course/{{$item->id}}" class=" h2 ">
-                    <img src="{{asset("storage/".$item->img)}}" alt="" class="category">
+                    {{-- <img src="{{asset("storage/".$item->img)}}" alt="" class="category"> --}}
+                    <img src="/{{$item->img}}" alt="" class="category">
                     <p class="text-center text-white text-decoration-none">{{$item->name}}</p>
                 </a>
             </div>
