2df961643ffc2c81fd740cba48567dc5e0af9cfd
Satenik <satabr1999@gmail.com>
2022-04-14 11:34:21 +0400

    first commit

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'մըsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
