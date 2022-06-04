d2e00f0ac06cf8394448d078371b36c59d6373ee
Satenik <satabr1999@gmail.com>
2022-06-05 00:55:26 +0400

    fixed bootstrap bugs

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -7,12 +7,23 @@
         @foreach ($test as $item)
         
             <div class="col-3  mt-1">
-                
+                @if ($item->id == 1)
                 <a href="/course/{{$item->id}}" class=" h2 ">
                     {{-- <img src="{{asset("storage/".$item->img)}}" alt="" class="category"> --}}
                     <img src="/{{$item->img}}" alt="" class="category">
                     <p class="text-center text-white text-decoration-none">{{$item->name}}</p>
                 </a>
+
+                @else
+
+                <a href="/file/course/{{$item->id}}.pdf" class=" h2 " target="_blank">
+                    {{-- <img src="{{asset("storage/".$item->img)}}" alt="" class="category"> --}}
+                    <img src="/{{$item->img}}" alt="" class="category">
+                    <p class="text-center text-white text-decoration-none">{{$item->name}}</p>
+                </a>
+
+                @endif
+                
             </div>
         @endforeach
         
