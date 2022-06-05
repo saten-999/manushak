736e9566765fd32dc42ec13be56fbd99b2e97766
Satenik <satabr1999@gmail.com>
2022-06-06 01:42:03 +0400

    changes

--- resources/views/subinfo.blade.php
+++ resources/views/subinfo.blade.php
@@ -3,27 +3,27 @@
 @section('content')
     <div class="container-fluid mt-5">
         <div class="row">
-            <div class="col-3 dasht">
+            <div class="col-lg-3 dasht">
                 <div>
                     <a href="/plan">Թեմատիկ պլանավորում</a>
                 </div>
             </div>
-            <div class="col-3 dasht">
+            <div class="col-lg-3 dasht">
                 <div>
                     <a href="{{asset('file/Դասիպլան.pdf') }}">Դասի պլան</a>
                 </div>
             </div>
-            <div class="col-3 dasht">
+            <div class="col-lg-3 dasht">
                 <div>
                     <a href="{{asset('file/Հաշվետվություն.pdf') }}">Հաշվետվության ձև</a>
                 </div>
             </div>
-            <div class="col-3 dasht">
+            <div class="col-lg-3 dasht">
                 <div>
                     <a href="{{asset('file/Դասալսմանբլանկ.pdf') }}">Դասալսման բլանկ</a>
                 </div>
             </div>
-            <div class="col-3 dasht">
+            <div class="col-lg-3 dasht">
                 <div>
                     <a href="/kisamyak">Կիսամյակայիններ</a>
                 </div>
