482b9c7ecb119a675a04a483c2697f233c6d39b7
Satenik <satabr1999@gmail.com>
2022-06-06 01:26:52 +0400

    changes

--- resources/views/subjectsss.blade.php
+++ resources/views/subjectsss.blade.php
@@ -8,7 +8,7 @@
             @if ($item->id<6)
             <div class="col-3   ">  
                 <div class="m-1 book">
-                    <a href="/{{$item->book}}" target="_blank" class="h2"> 
+                    <a href="/{{$item->book}}" target="_blank" class="h5"> 
                         <p class="text-center tex-twhite text-decoration-none">Ինֆորմատիկա </p>
                         <p class="text-center tex-twhite text-decoration-none">{{$item->class}}-րդ Դասարան </p>
                     </a>
